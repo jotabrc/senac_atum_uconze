f9a7cb2525cd742db9ffbfbc5961c0908d364aa3
jotabrc <ojcroveda@gmail.com>
2025-11-13 21:15:34 -0300

    feat: script insert produtos

---
+++ leiloes/src/main/resources/db/migration/V1_0_1__insert_produtos.sql
@@ -0,0 +1,4 @@
+INSERT INTO produtos (nome, valor, status) VALUES ('Produto 1', 199.90, 'A Venda');
+INSERT INTO produtos (nome, valor, status) VALUES ('Produto 2', 299.90, 'A Venda');
+INSERT INTO produtos (nome, valor, status) VALUES ('Produto 3', 399.90, 'Vendido');
+INSERT INTO produtos (nome, valor, status) VALUES ('Produto 4', 499.90, 'A Venda');
